c90e4b564289eca9cc1a10fc9d4db21681f58458
murderface <murzak.vv@gmail.com>
2015-09-07 15:06:46 +0300

    add first implementation of fields validator plugin

---
+++ fields_validator.js
@@ -0,0 +1,33 @@
+function validateEmail(fieldId, errorMessage = 'Wrong email'){
+    var emailPattern = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/i;
+    var valid = emailPattern.test($('#' + fieldId).val())
+    if (!valid) showErrorMessageFor(fieldId, errorMessage)
+    return valid
+  }
+
+function validatePresence(fieldId, errorMessage = 'Can\'t be blank'){
+  var valid = $('#' + fieldId).val().length > 0
+  if (!valid) showErrorMessageFor(fieldId, errorMessage)
+  return valid
+}
+
+function validateEqual(firstField, secondField, errorMessage = 'password doesn\'t match'){
+  var valid = $('#' + firstField).val() == $('#' + secondField).val();
+  if (!valid) showErrorMessageFor(firstField, errorMessage);
+  return valid;
+}
+
+function showErrorMessageFor(fieldId, errorText){
+  var error = errorText.toString();
+  var errorId = fieldId.toString() + '_error'
+  // we will show only one error message
+  if ($('#' + errorId).length > 0) return;
+  var errorDiv = '<div class="error-message" id="' +
+                  errorId + '">' +
+                  errorText.toString() + '</div>';
+  $('#' + fieldId).after(errorDiv)
+}
+
+function removeErrorMessageFor(fieldId){
+  $('#' + fieldId + '_error').remove();
+}
