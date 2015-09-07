f8b5846b006c8fb7a84b84f96f5cbee4467be0d7
murderface <murzak.vv@gmail.com>
2015-09-07 17:06:55 +0300

    remove validation message before validation

--- fields_validator.js
+++ fields_validator.js
@@ -1,4 +1,5 @@
 function validateEmail(fieldId, errorMessage = 'Wrong email'){
+    removeErrorMessageFor(fieldId);
     var emailPattern = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/i;
     var valid = emailPattern.test($('#' + fieldId).val())
     if (!valid) showErrorMessageFor(fieldId, errorMessage)
@@ -6,12 +7,14 @@ function validateEmail(fieldId, errorMessage = 'Wrong email'){
   }
 
 function validatePresence(fieldId, errorMessage = 'Can\'t be blank'){
+  removeErrorMessageFor(fieldId);
   var valid = $('#' + fieldId).val().length > 0
   if (!valid) showErrorMessageFor(fieldId, errorMessage)
   return valid
 }
 
 function validateEqual(firstField, secondField, errorMessage = 'password doesn\'t match'){
+  removeErrorMessageFor(firstField);
   var valid = $('#' + firstField).val() == $('#' + secondField).val();
   if (!valid) showErrorMessageFor(firstField, errorMessage);
   return valid;
